7d049dce4b6f9f2785b3761d4384c9e23b99bd03
Luke Floden <luke.floden@gmail.com>
2024-02-19 16:07:57 -0500

    chore(setup): setup tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-gray-800">Welcome to SvelteKit</h1>
+<p class="text-gray-700">
+	Visit
+	<a href="https://kit.svelte.dev">kit.svelte.dev</a>
+	to read the documentation
+</p>
