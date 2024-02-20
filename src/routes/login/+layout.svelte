7d90ce2f14ad73df07356c2d77c1079a19e7884a
Luke Floden <luke.floden@gmail.com>
2024-02-20 18:35:53 -0500

    refactor: playing with svelte and making a reactive variable that is set by function on each re-render

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+
+	$: currentStep = (() => {
+		const url: string = $page.route.id || '';
+		if (url.includes('photo')) {
+			return 3;
+		} else if (url.includes('username')) {
+			return 2;
+		}
+		return 1;
+	})();
 </script>
 
 <nav class="flex justify-center my-6">
 	<ul class="steps">
-		<a href="/login" class="step step-primary">Sign In</a>
-		<a
-			href="/login/username"
-			class="step"
-			class:step-primary={$page.route.id?.match(/username|photo/g)}
-		>
+		<a href="/login" class="step step-primary"> Sign In </a>
+		<a href="/login/username" class="step" class:step-primary={currentStep >= 2}>
 			Choose Username
 		</a>
-		<a href="/login/photo" class="step" class:step-primary={$page.route.id?.includes('photo')}>
-			Upload Photo
-		</a>
+		<a href="/login/photo" class="step" class:step-primary={currentStep === 3}> Upload Photo </a>
 	</ul>
 </nav>
 
 <main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
 	<div class="card-body items-center text-center">
 		<slot />
+		<h2>{currentStep}</h2>
 	</div>
 </main>
